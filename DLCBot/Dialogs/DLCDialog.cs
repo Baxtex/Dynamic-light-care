530348b38bb4c6f03572989849bbb43f05738df4
Strandberg95 <Strandberg.coffe@gmail.com>
2017-05-11 21:28:22 +0200

    app id and subscription added

---
+++ DLCBot/Dialogs/DLCDialog.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DLCBot.Dialogs
+{
+    [LuisModel("2ce20569-f378-4bea-93b2-651758dd3b26", "8af465818c3b400c9ea945a449fad81b")]
+
+    [Serializable]
+    public class DLCDialog : LuisDialog<object>
+    {
+        [LuisIntent("LightSwitch")]
+        public async Task ShowDevice(IDialogContext context, LuisResult result)
+        {
+             
+        }
+
+    }
+}
