41b9a8a243bfec7af7b218fd20dc4b4f72b631c2
Baxtex <anton.gustafsson@protonmail.com>
2017-05-12 00:19:42 +0200

    Added restsharp library and added Handler for it

---
+++ DLCBot/Ultilities/ApiHandler.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DLCBot.Ultilities
+{
+    public static class ApiHandler
+    {
+        private const string baseUrl = "http://dynamiclight.azurewebsites.net/api/1/";
+        private static readonly RestClient client;
+
+        static ApiHandler()
+        {
+            client = new RestClient();
+            client.BaseUrl = new Uri(baseUrl);
+        }
+
+        //Posts new conversations.
+        internal static void TurnLightsOn()
+        {
+            var request = new RestRequest("LightsOn", Method.POST);
+            var response = client.Execute(request);
+        }
+
+
+    }
+}
